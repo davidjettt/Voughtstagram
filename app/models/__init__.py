f28dcef7e31e80efa200c4b799f92c31ed0af775
Zach <hoschar.zach@gmail.com>
2022-08-21 14:00:25 -0500

    added some more seed data

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,2 @@
 from .db import db
-from .user import User, Post
+from .user import User, Post, Comment
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,4 +1,4 @@
-from app.models import db, User, Post
+from app.models import db, User, Post, Comment
 
 
 # Adds a demo user, you can add other users here if you want
@@ -7,7 +7,7 @@ def seed_users():
     marnie = User(username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(username='bobbie', email='bobbie@aa.io', password='password')
     homelander = User(username='homelander', email='homelander@vought.com', password='ryan')
-    starlight = User(username='starlight', email='anniejanuary@gmail.com', password='password')
+    starlight = User(username='therealstarlight', email='anniejanuary@gmail.com', password='password')
     vought_intl = User(username='voughtintl', email='voughthq@vought.com', password='password')
 
     db.session.add(demo)
@@ -21,29 +21,67 @@ def seed_users():
 
 def seed_posts():
     post1 = Post(
-        image_url='https://i.ytimg.com/vi/2iGcta6R_Nk/maxresdefault.jpg',
+        image_url='https://res.cloudinary.com/dpjpitop6/image/upload/v1661106766/maxresdefault_q8fish.jpg',
         description='Congratulations to the Deep for becoming Chief Sustainability Officer for Liquid Death!',
-        user_id=5
+        user_id=6
     )
 
     post2 = Post(
-        image_url='https://static.wikia.nocookie.net/amazons-the-boys/images/a/ab/Dawn-of-the-seven.jpg/revision/latest/scale-to-width-down/700?cb=20220520190954',
+        image_url='https://res.cloudinary.com/dpjpitop6/image/upload/v1661049206/281715475_401522891982495_494959663662951770_n_eeg6ru.jpg',
         description="We've heard your feedback and it's coming back for a limited time! Dawn of the Seven will be in theaters AGAIN next week",
-        user_id=5
+        user_id=6
     )
 
     post3 = Post(
-        image_url='https://www.highonfilms.com/wp-content/uploads/2022/07/The-Boys-Season-3-Episode-8-Recap-Ending-Explained.jpg',
+        image_url='https://res.cloudinary.com/dpjpitop6/image/upload/v1661106858/The-Boys-Season-3-Episode-8-Recap-Ending-Explained_cjiixk.jpg',
         description="A behind the scenes look at American Hero, airing tonight. Don't miss it!",
-        user_id=5
+        user_id=6
     )
 
+    post4 = Post(
+        image_url = 'https://res.cloudinary.com/dpjpitop6/image/upload/v1661106927/Homelander-The-Boys_npsavd.jpg',
+        description = 'Such a good pic of us, huh Starlight? 😍 #homelight',
+        user_id = 4
+    )
 
+    post5 = Post(
+        image_url = 'https://res.cloudinary.com/dpjpitop6/image/upload/v1661107286/the-boys3x07_0095_ycovid.jpg',
+        description = "Vought is getting desperate, but y'all have seen the truth. Don't let up! #voughtliedpeopledied",
+        user_id = 5
+    )
     db.session.add(post1)
     db.session.add(post2)
     db.session.add(post3)
+    db.session.add(post4)
+    db.session.add(post5)
 
     db.session.commit()
+
+
+def seed_comments():
+    comment1 = Comment(
+        comment = "Can't wait to watch this again, Homelander was so great!!",
+        user_id=2,
+        post_id=2
+    )
+
+    comment2 = Comment(
+        comment = "Unreal, this was all caught on video. Vought's days are numbered. #voughtliedpeopledied",
+        user_id=3,
+        post_id=5
+    )
+
+    comment3 = Comment(
+        comment = "Really hoping supersonic wins this!!",
+        user_id=1,
+        post_id=3
+    )
+
+    db.session.add(comment1)
+    db.session.add(comment2)
+    db.session.add(comment3)
+    db.session.commit()
+
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and RESET IDENTITY
@@ -55,4 +93,8 @@ def undo_users():
 
 def undo_posts():
     db.session.execute('TRUNCATE posts RESTART IDENTITY CASCADE;')
+    db.session.commit()
+
+def undo_comments():
+    db.session.execute('TRUNCATE comments RESTART IDENTITY CASCADE;')
     db.session.commit()
