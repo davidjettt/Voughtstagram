60155d74ab9b7f123225fb83c6e249ca9d5a5a04
allan <ayang740@gmail.com>
2022-08-23 13:00:05 -0400

    removed forms from app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,8 +11,6 @@ import { authenticate } from './store/session';
 import PostForm from './components/posts/PostForm';
 import { getAllPosts } from './store/posts';
 import Feed from './components/posts/PostFeed';
-import SinglePost from './components/posts/SinglePostModal/SinglePost';
-import EditForm from './components/posts/EditPost';
 import { loadCommentsThunk } from './store/comments';
 
 
@@ -57,9 +55,6 @@ function App() {
         </ProtectedRoute>
         <Route exact path="/feed">
           <Feed />
-        </Route>
-        <Route exact path="/feed/:postId/edit">
-          <EditForm />
         </Route>
         <ProtectedRoute path='/posts/new/' exact={true} >
           <PostForm />
