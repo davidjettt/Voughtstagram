d33fc3dc91f4e2107c51283cf8ae96ee780fb291
joon bae <wnsqoqo@gmail.com>
2022-08-23 12:10:08 -0500

    heart icon fix attempted

--- react-app/src/components/Likes/PostLikes.js
+++ react-app/src/components/Likes/PostLikes.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, Redirect, useHistory } from 'react-router-dom'
+import heartImage from '../../Images/svgexport-3.svg'
 import '../posts/post.css'
 
 export default function PostLikes( { postId }) {
 
     return (
         <div className="post-likes-container">
-        <img className="post-like-button" src="/Images/svgexport-3.svg" alt=''/>
+        <img className="post-like-button" src={ heartImage } alt=''/>
         </div>
     )
 }
