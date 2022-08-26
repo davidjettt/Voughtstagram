8df3fb8e071ac131f7f1a39e9e9f126159b4e7f6
Zach <hoschar.zach@gmail.com>
2022-08-26 17:04:52 -0500

    profile display css pretty much done

--- react-app/src/components/Profile/User.js
+++ react-app/src/components/Profile/User.js
@@ -7,6 +7,7 @@ import ShowFollowsModal from './ShowFollowsModal/ShowFollowsModal'
 import followingIcon from '../../../src/Images/followingicon.svg'
 import './profile.css'
 import FollowButton from './FollowButton';
+import defaultProfile from '../../../src/Images/defaultprofilepic.svg'
 
 function User() {
   const { userId } = useParams();
@@ -25,7 +26,7 @@ function User() {
   useEffect(() => {
     setFollowing(sessionUser?.following.includes(profileUser?.id))
     setNumberOfPosts(userPosts?.length)
-  }, [sessionUser, profileUser])
+  }, [sessionUser, profileUser, posts])
 
   if (!sessionUser || !profileUser) {
     return null;
@@ -33,35 +34,43 @@ function User() {
 
   return (
     <div className='profile-container'>
-      <div className='profile-header'>
-        <div>
-          {profileUser.username}
+      <div className='profile-header-container'>
+        <div className='profile-header-avatar-container'>
+
+          <div className='profile-header-avatar'>
+            <img className='profile-header-avatar-image' src={profileUser.avatar || defaultProfile} ></img>
+
+          </div>
         </div>
+        <div className='profile-header-info'>
+          <div className='profile-header-username'>
+            <span>{profileUser.username}</span>
+            {
+              profileUser &&
+              <FollowButton following={following} profileUserId={profileUser.id} />
+            }
+          </div>
 
-        {
-          profileUser &&
-          <FollowButton following={following} profileUserId={profileUser.id} />
-        }
+          <div className='profile-info-container'>
 
-      </div>
+            <div className='followers-display'>
+              <strong>{numberOfPosts && numberOfPosts}</strong> Posts
+            </div>
+            <div className='profile-info '>
+              {
+                profileUser &&
+                <ShowFollowsModal type='followers' list={profileUser.followers} />
+              }
+            </div>
+            <div className='profile-info'>
+              {
+                sessionUser &&
+                profileUser &&
+                <ShowFollowsModal type='following' list={profileUser.following} />
+              }
+            </div>
+          </div>
 
-      <div className='profile-info-container'>
-
-        <div className='profile-info'>
-          <strong>{numberOfPosts}</strong> Posts
-        </div>
-        <div className='profile-info '>
-          {
-            profileUser &&
-            <ShowFollowsModal type='followers' list={profileUser.followers} />
-          }
-        </div>
-        <div className='profile-info'>
-          {
-            sessionUser &&
-            profileUser &&
-            <ShowFollowsModal type='following' list={profileUser.following} />
-          }
         </div>
       </div>
       <div className='profile-post-body'>
