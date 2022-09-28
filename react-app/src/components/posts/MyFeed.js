fc2581289d6d4f06926f1142d3508848ca1d39c1
allan <ayang740@gmail.com>
2022-09-28 11:21:44 -0400

    show random suggested users

--- react-app/src/components/posts/MyFeed.js
+++ react-app/src/components/posts/MyFeed.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Feed from "./PostFeed";
+import defaultProfile from '../../../src/Images/defaultprofilepic.svg'
 
+import './post.css'
 
 export default function MyFeed() {
     const userId = useSelector(state => state.session.user.id)
@@ -16,7 +18,7 @@ export default function MyFeed() {
     let j = 0
 
     while (i--) {
-        j = Math.floor(Math.random() * (i+1));
+        j = Math.floor(Math.random() * (i));
         ranIds.push(userIds[j]);
         userIds.splice(j,1);
     }
@@ -38,20 +40,25 @@ export default function MyFeed() {
                 <div className="no-follows-container">
                     {/* <div className="empty-">You don't seem to be following anyone, check out the <strong><Link to="/feed">main feed</Link></strong> to find some content that you like.</div> */}
                     <div>
-                        <div>You don't seem to be following anyone, here are some suggested users to follow.</div>
-                        <Link to={`/users/${usersList[ranIds[0]]?.id}`}>
+                        <div className="suggested-users">You don't seem to be following anyone, here are some suggested users to follow.</div>
+                        <Link to={`/users/${usersList[ranIds[0]]?.id}`} className="suggested-user-link">
+                            <img className="suggested-user-image" src={usersList[ranIds[0]]?.avatar || defaultProfile} alt=''></img>
                             <div>{usersList[ranIds[0]]?.username}</div>
                         </Link>
-                        <Link to={`/users/${usersList[ranIds[1]]?.id}`}>
+                        <Link to={`/users/${usersList[ranIds[1]]?.id}`} className="suggested-user-link">
+                            <img className="suggested-user-image" src={usersList[ranIds[1]]?.avatar || defaultProfile} alt=''></img>
                             <div>{usersList[ranIds[1]]?.username}</div>
                         </Link>
-                        <Link to={`/users/${usersList[ranIds[2]]?.id}`}>
+                        <Link to={`/users/${usersList[ranIds[2]]?.id}`} className="suggested-user-link">
+                            <img className="suggested-user-image" src={usersList[ranIds[2]]?.avatar || defaultProfile} alt=''></img>
                             <div>{usersList[ranIds[2]]?.username}</div>
                         </Link>
-                        <Link to={`/users/${usersList[ranIds[3]]?.id}`}>
+                        <Link to={`/users/${usersList[ranIds[3]]?.id}`} className="suggested-user-link">
+                            <img className="suggested-user-image" src={usersList[ranIds[3]]?.avatar || defaultProfile} alt=''></img>
                             <div>{usersList[ranIds[3]]?.username}</div>
                         </Link>
-                        <Link to={`/users/${usersList[ranIds[4]]?.id}`}>
+                        <Link to={`/users/${usersList[ranIds[4]]?.id}`} className="suggested-user-link">
+                            <img className="suggested-user-image" src={usersList[ranIds[4]]?.avatar || defaultProfile} alt=''></img>
                             <div>{usersList[ranIds[4]]?.username}</div>
                         </Link>
                     </div>
